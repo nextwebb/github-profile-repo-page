5b61c2c9928294862e1d3891d25c222e546ec20b
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-05-21 16:22:01 +0100

    removed token

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const URL = "https://api.github.com/graphql";
 
 //token used with regex
 
-const mytoken   =   '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'
+const mytoken   =   ''
 
 fetch(URL, {
   method: "POST",
